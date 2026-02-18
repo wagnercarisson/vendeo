87e96d02f92e19cd2535ddd9ae8b25a816043617
wagnercarisson <wagnercarisson@gmail.com>
2026-02-17 23:29:27 -0300

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ padding: 40, fontFamily: "sans-serif" }}>
+      <h1>Vendeo</h1>
+      <p>Plataforma de campanhas para lojas</p>
+    </main>
+  )
+}
