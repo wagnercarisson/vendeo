03ec8e510ed125dea8f7583fbb2dad8dd411b39d
wagnercarisson <wagnercarisson@gmail.com>
2026-02-19 14:21:35 -0300

    Update page.tsx

--- app/campaigns/new/page.tsx
+++ app/campaigns/new/page.tsx
@@ -2,7 +2,7 @@
 
 import { useEffect, useState } from 'react';
 import { useRouter } from 'next/navigation';
-import { supabase } from '@/lib/supabase';
+import { supabase } from '../../../lib/supabase';
 
 type Store = {
   id: string;
